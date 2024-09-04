bd86aaea9fa4d2f3aafb3e9b66dfe35c64b1df23
Dmytro Lendiuk <dlend@softserveinc.com>
2024-09-04 18:40:32 +0300

    Added field with the DatePicker. Disables all past and weekend days

---
+++ YourContinentalTrack.Module/Controllers/VehicleLinkController.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Blazor.Editors;
+using YourContinentalTrack.Module.BusinessObjects;
+
+namespace YourContinentalTrack.Module.Controllers
+{
+    public partial class VehicleLinkController : ObjectViewController<DetailView, VehicleLink>
+    {
+        public VehicleLinkController()
+        {
+            InitializeComponent();
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+
+            View.CustomizeViewItemControl<DateTimePropertyEditor>(this, SetCalendarView, nameof(VehicleLink.EndDate));
+        }
+
+        private void SetCalendarView(DateTimePropertyEditor propertyEditor)
+        {
+            propertyEditor.ComponentModel.CustomDisabledDate += (e) =>
+            {
+                if (e.Date < DateTime.Today 
+                || e.Date.DayOfWeek == DayOfWeek.Saturday 
+                || e.Date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    e.IsDisabled = true;
+                }
+            };
+        }
+
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+        }
+    }
+}
